f9f812f6f13d8672bdd22aef9d332b380fce6a92
asd <asd>
2020-04-11 12:01:23 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//get random number
+	const passedQue = randomNumber(1, 100) > 8;
+	//if that numbe is in passed que range
+	if (!passedQue) {
+		quePageReload(8000);
+	}
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+function randomNumber(min: number, max: number): number {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+function quePageReload(delay: number): void {
+	setTimeout(function() {
+		location.reload();
+	}, delay);
 }
 
 export default App;
